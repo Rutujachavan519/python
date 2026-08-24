664651cc00a981635d09764cf99a106fe237b982
Rutujachavan519 <chavanrutuja519@gmail.com>
2026-08-24 13:36:19 +0530

    Update par3ex1.py

--- par3ex1.py
+++ par3ex1.py
@@ -1,10 +1,25 @@
-age=int(input("Enter your age :"))
-income=float(input("Enter annual family income:"))
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+income = float(input("Enter annual family income: "))
+caste = input("Enter your caste (SC/ST/OBC/General): ").upper()
 
-if age <25:
+if age < 25:
     if income < 300000:
-        print("Eligible for Specialized Education Scholarship")
+        if caste in ["SC", "ST", "OBC","VJT","NT"]:
+            print("Eligible for Specialized Education Scholarship")
+            result = "Eligible for Specialized Education Scholarship "
+        else:
+            print("Not Eligible: Caste is General")
+            result ="Not Eligible "
     else:
-        print("Not Eligible : Income is 300000 or above")
+        print("Not Eligible: Income is 300000 or above")
 else:
-    print("Not Eligible : Age must be below 25") 
+    print("Not Eligible: Age must be below 25")
+
+print("====================== Display ===================")
+print(" Name : ",name)
+print(" Age : ",age)
+print(" Income : ",income)
+print(" Caste : ",caste)
+print("Status : ",result)
+
