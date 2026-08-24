745e6c4e8bc25b06b2120e0bb8672bafcc7af3ec
Rutujachavan519 <chavanrutuja519@gmail.com>
2026-08-24 14:07:40 +0530

    Add files via upload

---
+++ par8ex1.py
@@ -0,0 +1,10 @@
+
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+
+first_name = first_name.upper().title()
+last_name = last_name.upper().title()
+
+full_name = first_name + " " + last_name
+
+print("Clean Full Name:", full_name)
